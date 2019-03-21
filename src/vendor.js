ff5d98436988bc019633754e47e94963153dc1ee
Paulo Rocha <paulocesar40@gmail.com>
2019-03-20 23:35:02 -0500

    second commit with many work

---
+++ src/vendor.js
@@ -0,0 +1,3 @@
+import LazySizes from 'lazysizes';
+//import ScrollReveal from 'scrollreveal';
+//import ScrollReveal from './../node_modules/scrollreveal/src/scrollreveal.js';
--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,24 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 const webpack = require("webpack");
+const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
 const TransferWebpackPlugin = require("transfer-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 //const ExtractTextPlugin = require("extract-text-webpack-plugin");
 //const devMode = process.env.NODE_ENV !== "production";
 
 module.exports = {
-  entry: [
-    //'webpack/hot/only-dev-server',
-    "tether",
-    "./src/app.js"
-  ],
+  entry: {
+    app: [
+      //'webpack/hot/only-dev-server',
+      "tether",
+      "font-awesome/scss/font-awesome.scss",
+      "./src/app.js"
+    ],
+    vendor: "./src/vendor.js"
+  },
   output: {
-    path: __dirname + "/dist",
+    path: path.resolve(__dirname, "./dist"),
     filename: "[name].bundle.js"
   },
   devServer: {
@@ -52,42 +57,90 @@ module.exports = {
       },
       {
         test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        use: "url-loader?limit=10000"
+        use: "url-loader?limit=10000&name=fonts/[name].[ext]"
       },
       {
         test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
-        use: "file-loader"
+        exclude: [/images/],
+        use: "file-loader?name=fonts/[name].[ext]"
       },
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
         use: [
-          "file-loader?name=images/[name].[ext]",
-          "image-webpack-loader?bypassOnDebug"
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[hash].[ext]",
+              outputPath: "images/",
+              publicPath: "images/"
+            }
+          }
+          /*{
+                loader: 'image-webpack-loader',
+                options: {
+                  bypassOnDebug: true,
+                  mozjpeg: {
+                    progressive: true,
+                    quality: 65
+                  },
+                  // optipng.enabled: false will disable optipng
+                  optipng: {
+                    enabled: false,
+                  },
+                  pngquant: {
+                    quality: '65-90',
+                    speed: 4
+                  },
+                  gifsicle: {
+                    interlaced: false,
+                  },
+                  // the webp option will enable WEBP
+                  webp: {
+                    quality: 75
+                  }
+                }
+            }*/
         ]
+      },
+      // font-awesome
+      {
+        test: /font-awesome\.config\.js/,
+        use: [{ loader: "style-loader" }, { loader: "font-awesome-loader" }]
+      },
+      // Bootstrap 4
+      {
+        test: /bootstrap\/dist\/js\/umd\//,
+        use: "imports-loader?jQuery=jquery"
       }
     ]
   },
   plugins: [
     new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
-      'window.jQuery': 'jquery',
-      tether: 'tether',
-      Tether: 'tether',
-      'window.Tether': 'tether',
-      Popper: ['popper.js', 'default'],
-      'window.Tether': 'tether',
-      Alert: 'exports-loader?Alert!bootstrap/js/dist/alert',
-      Button: 'exports-loader?Button!bootstrap/js/dist/button',
-      Carousel: 'exports-loader?Carousel!bootstrap/js/dist/carousel',
-      Collapse: 'exports-loader?Collapse!bootstrap/js/dist/collapse',
-      Dropdown: 'exports-loader?Dropdown!bootstrap/js/dist/dropdown',
-      Modal: 'exports-loader?Modal!bootstrap/js/dist/modal',
-      Popover: 'exports-loader?Popover!bootstrap/js/dist/popover',
-      Scrollspy: 'exports-loader?Scrollspy!bootstrap/js/dist/scrollspy',
-      Tab: 'exports-loader?Tab!bootstrap/js/dist/tab',
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery",
+      tether: "tether",
+      Tether: "tether",
+      "window.Tether": "tether",
+      Popper: ["popper.js", "default"],
+      "window.Tether": "tether",
+      Alert: "exports-loader?Alert!bootstrap/js/dist/alert",
+      Button: "exports-loader?Button!bootstrap/js/dist/button",
+      Carousel: "exports-loader?Carousel!bootstrap/js/dist/carousel",
+      Collapse: "exports-loader?Collapse!bootstrap/js/dist/collapse",
+      Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown",
+      Modal: "exports-loader?Modal!bootstrap/js/dist/modal",
+      Popover: "exports-loader?Popover!bootstrap/js/dist/popover",
+      Scrollspy: "exports-loader?Scrollspy!bootstrap/js/dist/scrollspy",
+      Tab: "exports-loader?Tab!bootstrap/js/dist/tab",
       Tooltip: "exports-loader?Tooltip!bootstrap/js/dist/tooltip",
-      Util: 'exports-loader?Util!bootstrap/js/dist/util'
+      Util: "exports-loader?Util!bootstrap/js/dist/util"
+    }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "[name].css",
+      chunkFilename: "[id].css"
     }),
     new HtmlWebpackPlugin({
       title: "We Mean Web",
@@ -97,14 +150,32 @@ module.exports = {
       filename: "index.html",
       template: "src/index.html"
     }),
-    new MiniCssExtractPlugin({
-      // Options similar to the same options in webpackOptions.output
-      // both options are optional
-      filename: "[name].css",
-      chunkFilename: "[id].css"
-    }),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NamedModulesPlugin(),
-    new CleanWebpackPlugin(["dist"]),
+    new BrowserSyncPlugin(
+      {
+        // browse to http://localhost:3000/ during development,
+        // ./public directory is being served
+        //notify: true,
+        host: "localhost",
+        port: 3000,
+        proxy: "http://localhost:8080/",
+        files: [
+          {
+            match: ["**/*.html"],
+            fn: function(event, file) {
+              if (event === "change") {
+                const bs = require("browser-sync").get("bs-webpack-plugin");
+                bs.reload();
+              }
+            }
+          }
+        ]
+      },
+      {
+        reload: true
+      }
+    ),
+    new CleanWebpackPlugin(["dist"])
   ]
 };
